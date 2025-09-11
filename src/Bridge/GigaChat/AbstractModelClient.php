63dc0f98772c121fe83aaf83d2a99ff297ecf25f
Al Ganiev <helios.ag@gmail.com>
2025-09-11 14:34:43 +1000

    Added base modelclient

---
+++ src/Bridge/GigaChat/AbstractModelClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FM\AI\Platform\Bridge\GigaChat;
+
+use Symfony\AI\Platform\Exception\InvalidArgumentException;
+
+
+abstract readonly class AbstractModelClient
+{
+    protected static function getBaseUrl(): string
+    {
+        return 'https://gigachat.devices.sberbank.ru/api/v1/';
+
+    }
+
+    protected static function getOauthUrl(): string
+    {
+        return 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth';
+
+    }
+
+    protected static function validateApiKey(string $apiKey): void
+    {
+        if ('' === $apiKey) {
+            throw new InvalidArgumentException('The API key must not be empty.');
+        }
+    }
+}
